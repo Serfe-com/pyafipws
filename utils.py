cf62e3c15c0a5b6a0e69455f45bc7f1551fa5a58
Gerónimo Rivoira <geronimo@serfe.com>
2026-03-02 18:08:05 -0300

    fix: actualizar generación de contexto ssl

--- utils.py
+++ utils.py
@@ -66,8 +66,8 @@ try:
         def _build_ssl_context_new(*args, **kwargs):
             context = _build_ssl_context(*args, **kwargs)
             # fix ssl.SSLError: [SSL: DH_KEY_TOO_SMALL] dh key too small
-            # alternative: context.set_ciphers("DEFAULT@SECLEVEL=1")
-            context.set_ciphers("AES128-SHA")
+            context.set_ciphers("DEFAULT@SECLEVEL=1")
+            #context.set_ciphers("AES128-SHA")
             return context
 
         httplib2._build_ssl_context = _build_ssl_context_new
