ac3ce22053b633ee6417bc92addfe33af93ebb4a
Gerónimo Rivoira <geronimo@serfe.com>
2026-03-03 07:04:00 -0300

    debug: agregar stdout

--- utils.py
+++ utils.py
@@ -60,16 +60,20 @@ try:
 
     monkey_patch = sys.version_info < (3, ) or httplib2._build_ssl_context.__module__ != "httplib2"
     needs_patch = platform.system() == 'Linux' or sys.version_info > (3, 10)
-    _build_ssl_context = httplib2._build_ssl_context
+    if needs_patch and not monkey_patch:
+        _build_ssl_context = httplib2._build_ssl_context
 
-    def _build_ssl_context_new(*args, **kwargs):
-        context = _build_ssl_context(*args, **kwargs)
-        # fix ssl.SSLError: [SSL: DH_KEY_TOO_SMALL] dh key too small
-        context.set_ciphers("DEFAULT@SECLEVEL=1")
-        #context.set_ciphers("AES128-SHA")
-        return context
+        def _build_ssl_context_new(*args, **kwargs):
+            context = _build_ssl_context(*args, **kwargs)
+            # fix ssl.SSLError: [SSL: DH_KEY_TOO_SMALL] dh key too small
+            context.set_ciphers("DEFAULT@SECLEVEL=1")
+            # context.set_ciphers("AES128-SHA")
+            return context
 
-    httplib2._build_ssl_context = _build_ssl_context_new
+        httplib2._build_ssl_context = _build_ssl_context_new
+
+    print("corregido contexto ssl")
+
 
 except ImportError as mnfe:
     if "httplib2" in str(mnfe):
